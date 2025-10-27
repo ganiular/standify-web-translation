9beab50d5d0082719d81dbcc7decf616cc71e783
ganiular <ganiular@gmail.com>
2025-10-27 21:40:00 +0100

    change in translation

--- en.ts
+++ en.ts
@@ -1,6 +1,6 @@
 const en = {
     Header: {
-        buttonHomePage: "Home Page",
+        buttonHomePage: "Home",
         buttonMarketPlace: "Marketplace",
         buttonEventPage: "Explore Event",
         buttonPostService: "Post Service",
