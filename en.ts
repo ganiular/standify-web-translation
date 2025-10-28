4edd5f8ca26c0858059f31abd8618ecce4580728
ganiular <ganiular@gmail.com>
2025-10-28 12:02:55 +0100

    update to notify standify with commit id

--- en.ts
+++ en.ts
@@ -1,6 +1,6 @@
 const en = {
     Header: {
-        buttonHomePage: "Home Page",
+        buttonHomePage: "Home Pages",
         buttonMarketPlace: "Marketplace",
         buttonEventPage: "Explore Event",
         buttonPostService: "Post Service",
