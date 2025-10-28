0bb2ca22f125270228d034dda9c95f50310ac418
ganiular <ganiular@gmail.com>
2025-10-28 13:03:33 +0100

    change

--- en.ts
+++ en.ts
@@ -1,6 +1,6 @@
 const en = {
     Header: {
-        buttonHomePage: "Home Update!",
+        buttonHomePage: "Home!",
         buttonMarketPlace: "Marketplace",
         buttonEventPage: "Explore Event",
         buttonPostService: "Post Service",
